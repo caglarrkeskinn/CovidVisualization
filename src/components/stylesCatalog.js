9e92a11e2ded3844f079a5da7080f3105d60ebab
Çağlar Keskin <caglarkeskin@Caglar-MacBook-Pro.local>
2023-06-13 17:37:02 +0300

    Statistics Dataset is done

--- src/components/stylesCatalog.js
+++ src/components/stylesCatalog.js
@@ -1,276 +1,255 @@
 import { StyleSheet } from "react-native";
 
 export const stylesCatalog = StyleSheet.create({
-      mapButton: {
-        borderRadius: 50,
-        backgroundColor: "#35464F",
-        position: "absolute",
-        top: "5%",
-        right: "5%",
-        width:  80,
-        height: 45,
-       
-      },
-      container: {
-        flex: 1,
-        backgroundColor: "white",
-      },
-      cardStyle:{
-        
-        margin: 10,
-        borderWidth:1,
-        borderColor:'gray',
-      },
-      scrollContainer:{
-        marginBottom:150
-      },
-      cardImage:{
-        height: 135, 
-        width: '100%'
-      },
-      container2: {
-        height: 100,
-        width:  164,
-        borderRadius: 30,
-        padding: 15,
-        marginLeft: 20,
-        marginBottom: 12,
-        
-    },
-      
-      homeButton: {
-        borderRadius: 30,
-        backgroundColor: "#3F414E",
-        position: "absolute",
-        top: "75%",
-        justifyContent:'center',
-        position:"absolute",
-        width:  140,
-        height: 45,
-      },
-      covidHeading: {
-        color: "#FFF",
-        fontSize: 20,
-        alignSelf: "center",
-        fontWeight: "bold",
-        margin: 25,
-        marginTop: "10%",
-        
-      },
-      notification: {
-        color: "#FFF",
-        fontSize: 16,
-        alignSelf: "flex-end",
-        fontWeight: "bold",
-        margin: 20,
-        marginTop: "15%",
-        
-      },
-      homeButtonHeading: {
-        
-        color: "white",
-        position:"absolute",
-        fontSize: 16,
-        alignSelf: "center",
-        fontWeight: "bold",    
-        margin: 10,
-      },
-    
-      cards: {
-        marginTop: -120,
-      },
-      casesHeading: {
-        marginTop: 20,
-    
-        color: "black",
-        fontSize: 15,
-        alignSelf: "center",
-        fontWeight: "bold",
-        textAlign: "center",
-      },
-      flatList: {
-      marginTop: 10,
-      borderTopWidth:2,
-      borderColor:'black'
-      
-      },
-      boxStatistics: {
-        borderWidth: 2,
-        borderColor: "#27546C",
-        alignItems: "center",
-        textAlign: "center",
-        borderBottomLeftRadius: 25,
-        borderBottomRightRadius: 25,
-        backgroundColor: "#27546C",
-        height: 169,
-      },
-      boxHome: {
-        borderWidth: 2,
-        borderColor: "#27546C",
-        alignItems: "center",
-        textAlign: "center",
-        borderBottomLeftRadius: 25,
-        borderBottomRightRadius: 25,
-        backgroundColor: "#27546C",
-        height: 299,
-      },
-      boxNews: {
-        borderWidth: 2,
-        borderColor: "#27546C",
-        alignItems: "center",
-        textAlign: "center",
-        borderBottomLeftRadius: 25,
-        borderBottomRightRadius: 25,
-        backgroundColor: "#27546C",
-        height: 120,
-      },
-    
-    
-      boxCard:{
-        borderWidth: 2,
-        borderColor: "white",
-        width: 171,
-        height: 184,
-        alignItems: "center",
-        justifyContent:'center',
-        textAlign: "center",
-        borderRadius:25,
-        backgroundColor: "#27546C",
-        borderWidth:1,
-        borderColor:'gray',
-        left: 211,
-        top: 10
-      },
-      map: {
-        flex:1,
-        width: "100%",
-        height: "100%",
-      },
-      Global:{
-        borderRadius: 50,
-        backgroundColor: "#FFFFFF",
-        position: "absolute",
-        width: 126,
-        height: 31,
-        top: 120,
-        left: 200,
-    
-      },
-      globalText: {
-        //fontFamily: 'Roboto',
-        color: 'black',
-        position: "absolute",
-        fontSize: 20,
-        alignSelf: "center",
-        fontWeight: "bold",
-        margin: 3,
-      },
-      Turkey:{
-        borderRadius: 50,
-        backgroundColor: '#263238',
-        position: "absolute",
-        width:  241,
-        height: 31,
-        top: 120,
-        left: 85,
-    
-      },
-      turkeyText: {
-        //fontFamily: 'Roboto',
-        position: "absolute",
-        color: 'white',
-        fontSize: 20,
-        alignSelf: "flex-start",
-        fontWeight: "bold",
-        paddingLeft: 25,
-        margin: 3,
-      },
-      rows: {
-        width: '100%',
-        marginTop: 10,
-        marginBottom: 8,
-        padding: 10,
-        
-      },
-      countryName: {
-          fontSize: 15,
-          color:'black',
-          fontWeight: 'bold'
-      },
-      totalCases: {
-          fontSize: 16,
-          color:'black',
-          fontWeight: 'bold',
-          marginTop: 5
-      },
-      flag: {
-          height: 30,
-          width: 40,
-          padding: 10, 
-          borderRadius: 1000
-      },
-      
-      col: {
-          flexDirection: "row",
-          marginBottom: 45
-      },
-      cardsTitle: {
-          alignSelf: "center",
-          position:"absolute",
-          marginTop: 5,
-          color: 'white',
-          fontWeight: "bold",
-          flexShrink: 12,
-          fontSize: 16,
-          
-      },
-      titleBigBox: {
-        alignSelf: "center",
-        position:"absolute",
-        marginTop: 5,
-        color: '#533219',
-        fontWeight: "bold",
-        //flexShrink: 12,
-        fontSize: 16,
-        
-    },
-      number: {
-        
-          fontWeight: '500',
-          color: 'white',
-          justifyContent:'center',
-          alignSelf:'center',
-          marginTop:0
-            
-      },
-      preventionText:{
-        marginTop:5,
-        alignSelf:'flex-start',
-        padding:10,
-        fontWeight:'bold',
-        fontSize: 22,
-        color: 'black',
-      },
-      articleText:{
-        marginTop:5,
-        alignSelf:'flex-start',
-        padding:10,
-        fontWeight:'bold',
-        fontSize: 22,
-        color: 'black',
-      },
-      tabBarStyle:{
-        position:'absolute',
-        borderRadius:15,
-        
+  mapButton: {
+    borderRadius: 50,
+    backgroundColor: "#35464F",
+    position: "absolute",
+    top: "5%",
+    right: "5%",
+    width: 80,
+    height: 45,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  cardStyle: {
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+  scrollContainer: {
+    marginBottom: 150,
+  },
+  cardImage: {
+    height: 135,
+    width: "100%",
+  },
+  container2: {
+    height: 100,
+    width: 164,
+    borderRadius: 30,
+    padding: 15,
+    marginLeft: 20,
+    marginBottom: 12,
+  },
 
-        
+  homeButton: {
+    borderRadius: 30,
+    backgroundColor: "#3F414E",
+    position: "absolute",
+    top: "75%",
+    justifyContent: "center",
+    position: "absolute",
+    width: 140,
+    height: 45,
+  },
+  covidHeading: {
+    color: "#FFF",
+    fontSize: 20,
+    alignSelf: "center",
+    fontWeight: "bold",
+    margin: 25,
+    marginTop: "10%",
+  },
+  notification: {
+    color: "#FFF",
+    fontSize: 16,
+    alignSelf: "flex-end",
+    fontWeight: "bold",
+    margin: 20,
+    marginTop: "15%",
+  },
+  homeButtonHeading: {
+    color: "white",
+    position: "absolute",
+    fontSize: 16,
+    alignSelf: "center",
+    fontWeight: "bold",
+    margin: 10,
+  },
 
+  cards: {
+    marginTop: -120,
+  },
+  casesHeading: {
+    marginTop: 20,
 
-      }
+    color: "black",
+    fontSize: 15,
+    alignSelf: "center",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  flatList: {
+    flex: 1,
+    alignItems: "stretch",
+    marginTop: 10,
+    borderTopWidth: 2,
+    borderColor: "black",
+  },
+  boxStatistics: {
+    borderWidth: 2,
+    borderColor: "#27546C",
+    alignItems: "center",
+    textAlign: "center",
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    backgroundColor: "#27546C",
+    height: 169,
+  },
+  boxHome: {
+    borderWidth: 2,
+    borderColor: "#27546C",
+    alignItems: "center",
+    textAlign: "center",
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    backgroundColor: "#27546C",
+    height: 299,
+  },
+  boxNews: {
+    borderWidth: 2,
+    borderColor: "#27546C",
+    alignItems: "center",
+    textAlign: "center",
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    backgroundColor: "#27546C",
+    height: 120,
+  },
 
-      
-      
+  boxCard: {
+    borderWidth: 2,
+    borderColor: "white",
+    width: 171,
+    height: 184,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    borderRadius: 25,
+    backgroundColor: "#27546C",
+    borderWidth: 1,
+    borderColor: "gray",
+    left: 211,
+    top: 10,
+  },
+  map: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  Global: {
+    borderRadius: 50,
+    backgroundColor: "#FFFFFF",
+    position: "absolute",
+    width: 126,
+    height: 31,
+    top: 120,
+    left: 200,
+  },
+  globalText: {
+    //fontFamily: 'Roboto',
+    color: "black",
+    position: "absolute",
+    fontSize: 20,
+    alignSelf: "center",
+    fontWeight: "bold",
+    margin: 3,
+  },
+  Turkey: {
+    borderRadius: 50,
+    backgroundColor: "#263238",
+    position: "absolute",
+    width: 241,
+    height: 31,
+    top: 120,
+    left: 85,
+  },
+  turkeyText: {
+    //fontFamily: 'Roboto',
+    position: "absolute",
+    color: "white",
+    fontSize: 20,
+    alignSelf: "flex-start",
+    fontWeight: "bold",
+    paddingLeft: 25,
+    margin: 3,
+  },
+  rows: {
+    width: "100%",
+    marginTop: 10,
+    marginBottom: 8,
+    padding: 10,
+  },
+  countryName: {
+    fontSize: 15,
+    color: "black",
+    fontWeight: "bold",
+  },
+  totalCases: {
+    fontSize: 16,
+    color: "black",
+    fontWeight: "bold",
+    marginTop: 5,
+  },
+  flag: {
+    height: 30,
+    width: 40,
+    padding: 10,
+    borderRadius: 1000,
+  },
+
+  col: {
+    flexDirection: "row",
+    marginBottom: 45,
+  },
+  cardsTitle: {
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 5,
+    color: "white",
+    fontWeight: "bold",
+    flexShrink: 12,
+    fontSize: 16,
+  },
+  titleBigBox: {
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 5,
+    color: "#533219",
+    fontWeight: "bold",
+    //flexShrink: 12,
+    fontSize: 16,
+  },
+  number: {
+    fontWeight: "500",
+    color: "white",
+    justifyContent: "center",
+    alignSelf: "center",
+    marginTop: 0,
+  },
+  preventionText: {
+    marginTop: 5,
+    alignSelf: "flex-start",
+    padding: 10,
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "black",
+  },
+  articleText: {
+    marginTop: 5,
+    alignSelf: "flex-start",
+    padding: 10,
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "black",
+  },
+  tabBarStyle: {
+    position: "absolute",
+    borderRadius: 15,
+  },
 });
 
 export default stylesCatalog;
--- src/screens/Statistics.js
+++ src/screens/Statistics.js
@@ -5,7 +5,7 @@ import {
   Text,
   StyleSheet,
   FlatList,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 import stylesCatalog from "../components/stylesCatalog";
 import { ScrollView } from "react-native-gesture-handler";
@@ -14,150 +14,189 @@ import ItemRows from "../components/ItemRows";
 import MapView, { Heatmap } from "react-native-maps";
 import * as Location from "expo-location";
 import SwitchSelector from "react-native-switch-selector";
-import  SQLite  from "react-native-sqlite-storage";
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
- 
-
-
+import SQLite from "react-native-sqlite-storage";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import supabase from "../lib/supabase";
 
 const Statistics = () => {
+  const [casesData, setCasesData] = useState([]);
+  const [trcasesData, settrCasesData] = useState([]);
 
-// Flatlist data 
-const ItemRows = ({ item }) => {
-  return (
+  useEffect(() => {
+    getCases();
+  }, []);
+
+  useEffect(() => {
+    gettrCases();
+  }, []);
+
+  async function getCases() {
+    try {
+      let { data, error, status } = await supabase
+        .from("cases")
+        .select("Country_Region, Deaths");
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setCasesData(data);
+      }
+    } catch (error) {
+      console.error("Veri çekme hatası:", error);
+    }
+  }
+
+  async function gettrCases() {
+    try {
+      let { data, error, status } = await supabase
+        .from("trcases")
+        .select("region, trnumber");
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        settrCasesData(data);
+      }
+    } catch (error) {
+      console.error("Veri çekme hatası:", error);
+    }
+  }
+
+  const ItemRowsComponent = ({ item }) => {
+    return (
       <View style={stylesCatalog.rows}>
-          <View style={{flexDirection: 'row',justifyContent: 'space-around'}}>
-              
-              <View style={{ marginRight: 100, marginTop: 5 }}>
-                  <Text style={stylesCatalog.countryName}>{item.Countries}</Text>
-              </View>
-              <View>
-                  <Text style={stylesCatalog.totalCases}>{item.n}</Text>
-              </View>
-          </View>
-      </View>
-  );
-}
-const turkeyCases = [] ;
- turkeyCases.push({Countries:'Amasya' , n:1500});
- turkeyCases.push({Countries:'Bursa' , n:2415});
-
-
-
-const globalCases = [] ;
-globalCases.push({Countries:'Brazil' , n:150020020});
-globalCases.push({Countries:'German' , n:24154564});
-
-const totalCases = [] ;
-totalCases.push(1232132);
-const recovered = [] ;
-recovered.push(4545);
-const [par, setPar] = useState(true);
-
- 
-  return (
-    <View style={stylesCatalog.container}>
-  
-          <View style={stylesCatalog.boxStatistics}>
-            <Text style={stylesCatalog.covidHeading}>Statistics</Text>
-                <SwitchSelector
-                initial={0}
-                onPress={value => setPar(value)}
-                textColor= 'black'
-                selectedColor='white'
-                buttonColor='#35464F'
-                borderColor='white'
-                hasPadding
-                options={[
-                  { label: "Turkey", value: true },
-                  { label: "Global", value: false} 
-                ]}
-                testID="gender-switch-selector"
-                accessibilityLabel="gender-switch-selector"
-                bold
-                style={{width:250 , marginTop:10}}
-                height={31}
-                fontSize={16}
-                
-                
-              />
-          </View>
-          
-          <View style={stylesCatalog.boxCard}>
-                <View style={{alignItems:'center'}}> 
-                
-                  <Text style={{ fontSize:22,fontWeight: "600",marginTop:5,color: 'white',}}>Total Cases</Text>
-                 
-                  <Text style={{ fontSize:20,fontWeight: "400",marginTop:15,color: 'white'}}>{totalCases}</Text>
-                  
-                </View>
-                <View>
-                <MaterialCommunityIcons name="chart-bar"   marginTop={27} color={color='white'} size={size=60 }/>
-                </View>
-
-            </View> 
-          
-
-          <View style={stylesCatalog.cards}>
-            <ScrollView
-              // horizontal
-              //  showsHorizontalScrollIndicator={false}
-              style={{ marginTop: -55 }}
-            >
-               {/* <Cards  
-                
-                title="Total Cases"
-                bg="#db3939"
-                number={totalCases}
-              />  */}
-
-              <Cards
-              
-
-                title="Recovered"
-                bg="#27546C"
-                number={recovered}
-              
-              /> 
-
-              <Cards
-                title="Death Reported"
-                bg="#27546C"
-                number={recovered}
-              /> 
-              
-            </ScrollView>
+        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+          <View style={{ marginRight: 100, marginTop: 5 }}>
+            <Text style={stylesCatalog.countryName}>
+              {item["Country_Region"]}
+            </Text>
           </View>
           <View>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text style={stylesCatalog.casesHeading}>Covid Cases by region</Text>
-            </View>
+            <Text style={stylesCatalog.totalCases}>{item["Deaths"]}</Text>
           </View>
-          
-          <View style={stylesCatalog.flatList}>
-          {par === true &&(
-          <FlatList
-            data={turkeyCases}
-            renderItem={({ item }) => <ItemRows item={item} />}
-            keyExtractor={(item, index) => index.toString()}
-          />)}
-          {par === false &&(
-          <FlatList
-            data={globalCases}
-            renderItem={({ item }) => <ItemRows item={item} />}
-            keyExtractor={(item, index) => index.toString()}
-          />)}
+        </View>
+      </View>
+    );
+  };
+
+  const ItemRowsComponentTr = ({ item }) => {
+    return (
+      <View style={stylesCatalog.rows}>
+        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+          <View style={{ marginRight: 100, marginTop: 5 }}>
+            <Text style={stylesCatalog.countryName}>{item["region"]}</Text>
           </View>
-          
-  
+          <View>
+            <Text style={stylesCatalog.totalCases}>{item["trnumber"]}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const totalCases = [1232132];
+  const recovered = [4545];
+  const [par, setPar] = useState(true);
+
+  return (
+    <View style={stylesCatalog.container}>
+      <View style={stylesCatalog.boxStatistics}>
+        <Text style={stylesCatalog.covidHeading}>Statistics</Text>
+        <SwitchSelector
+          initial={0}
+          onPress={(value) => setPar(value)}
+          textColor="black"
+          selectedColor="white"
+          buttonColor="#35464F"
+          borderColor="white"
+          hasPadding
+          options={[
+            { label: "Turkey", value: true },
+            { label: "Global", value: false },
+          ]}
+          testID="gender-switch-selector"
+          accessibilityLabel="gender-switch-selector"
+          bold
+          style={{ width: 250, marginTop: 10 }}
+          height={31}
+          fontSize={16}
+        />
+      </View>
+
+      <View style={stylesCatalog.boxCard}>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: "600",
+              marginTop: 5,
+              color: "white",
+            }}
+          >
+            Total Cases
+          </Text>
+
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "400",
+              marginTop: 15,
+              color: "white",
+            }}
+          >
+            {totalCases}
+          </Text>
+        </View>
+        <View>
+          <MaterialCommunityIcons
+            name="chart-bar"
+            marginTop={27}
+            color={(color = "white")}
+            size={(size = 60)}
+          />
+        </View>
+      </View>
+
+      <View style={stylesCatalog.cards}>
+        <ScrollView style={{ marginTop: -55 }}>
+          <Cards title="Recovered" bg="#27546C" number={recovered} />
+          <Cards title="Death Reported" bg="#27546C" number={recovered} />
+        </ScrollView>
+      </View>
+
+      <View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={stylesCatalog.casesHeading}>Covid Cases by region</Text>
+        </View>
+      </View>
+
+      <View style={stylesCatalog.flatList}>
+        {par === true && (
+          <FlatList
+            data={trcasesData}
+            renderItem={({ item }) => <ItemRowsComponentTr item={item} />}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        )}
+        {par === false && (
+          <FlatList
+            data={casesData}
+            renderItem={({ item }) => <ItemRowsComponent item={item} />}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        )}
+      </View>
     </View>
-    
   );
 };
 
